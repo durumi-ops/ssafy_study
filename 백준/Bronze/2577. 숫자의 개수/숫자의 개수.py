19c6dccebb492f586df6ab3f2cef4a9b9d509775
durumi-ops <durumi0219@gmail.com>
2026-02-12 09:05:31 +0900

    [Bronze II] Title: 숫자의 개수, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
@@ -0,0 +1,18 @@
+# 입력값을 받는다.
+A = int(input())
+B = int(input())
+C = int(input())
+
+sum = (A*B*C)
+a = list(map(int,str(sum)))
+
+print(a.count(0))
+print(a.count(1))
+print(a.count(2))
+print(a.count(3))
+print(a.count(4))
+print(a.count(5))
+print(a.count(6))
+print(a.count(7))
+print(a.count(8))
+print(a.count(9))
