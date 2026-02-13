43b97c8f9736bdadc300af4a63f0caff1391d5c1
durumi-ops <durumi0219@gmail.com>
2026-02-13 15:30:36 +0900

    [Bronze II] Title: 문자열 반복, Time: 28 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.py
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -1,13 +1,8 @@
-T= int(input())
-
-p=[]
+T = int(input())
 for i in range(T):
-    ans = []
-    num, chr = input().split()
-    chr_list = list(chr)
-    for i in range(len(chr_list)):
-        ans.append(chr_list[i]*int(num))
-    p.append(''.join(ans))
-
-for i in range(len(p)):
-    print(p[i])
+    list = []
+    S,R = input().split()  # 숫자와 문자를 string으로 입력받는다
+    num = int(S)
+    for ch in R:
+        list.append(ch*num)
+    print(''.join(list))
